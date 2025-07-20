50e4db72fed4503388997906fbd9d3978ba89847
Harshitkhetawat25 <harshitkhetawat25@gmail.com>
2025-07-20 23:57:03 +0530

    Home page done

---
+++ src/landing_page/OpenAccount.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function OpenAccount() {
+    return (
+          <div className='container p-5 text-center'>
+            <div className='row'>
+              
+            <h1 className='mt-5 fs-3'>Open a Zerodha account</h1>
+            <p className='text-muted mt-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+            <button className='p-2 mt-3 btn btn-primary fs-5 text-white mb-5' style={{width:"18%",margin:"0 auto"}}>Signup For Free</button>
+
+            </div>
+
+
+        </div>
+    );
+}
+
+export default OpenAccount;
