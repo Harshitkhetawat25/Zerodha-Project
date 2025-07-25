42621812c78b1c11f385f28a8e23232131016179
Harshit <harshitkhetawat25@gmail.com>
2025-07-26 00:44:41 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,43 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 8080;
+const DB_URL = process.env.MONGO_URL;
+const {HoldingModel} = require("./model/HoldingModel");
+mongoose.set('strictQuery', true);
 
-const app = express() 
 
-app.listen(8080, () =>{
-    console.log('Server is running on port 8080')
+
+app.get('/addHoldings', async(req, res)=>{
+    let tempHoldings = [{
+    name: "BHARTIARTL",
+    qty: 2,
+    avg: 538.05,
+    price: 541.15,
+    net: "+0.58%",
+    day: "+2.99%",
+    }];
+    tempHoldings.forEach((item)=>{
+        let newHolding = new HoldingModel({
+            name:item.name,
+            qty:item.qty,
+            avg:item.avg,
+            price:item.price,
+            net:item.net,
+            day:item.day,
+        });
+
+        newHolding.save();
+    })
+    res.send("Holdings Added");
 })
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+  mongoose.connect(DB_URL).then(() => {
+    console.log("Connected to MongoDB");
+  }).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+});
